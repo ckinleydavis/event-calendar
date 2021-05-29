d5cb5c1209f7b4a9ebfb28220e1a1f80d689eb13
Kinley Davis <d8tascientist@gmail.com>
2021-05-28 21:38:27 -0400

    update date position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 // Get Current Date
 // REF: https://momentjs.com/docs/#/displaying/format/
-var current = moment().format("dddd, MMMM Do YYYY");
-console.log("The Current Day and Time: " + current);
+// REF: Week 04 - Activity 09 - Time Intervals
+    // TESTING
+    // console.log("The Current Day: " + current);
+    // console.log("The Current Day Element: " + currentDate);
+var current = moment().format("dddd, MMMM DD, YYYY");
+var currentDate = document.querySelector(".currentDate");
+currentDate.textContent = current;
+
+// Event Element & Submit
+// REF: Week 05, Activity - Form Elements
+var calendarEl = $('#calendar-form');
+var eventEl = $('input[name="event-data"]');
+
+function handleFormSubmit(event) {
+    // Prevent the default behavior
+    event.preventDefault();
+  
+    console.log('Event Data:', eventEl.val());
+}
+
+// Event Submit
+eventEl.on('submit', handleFormSubmit);
